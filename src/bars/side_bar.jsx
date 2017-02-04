632cf31ffb1ae7c7bba11adb2f474334ac89742e
Eitan <eitanakman@gmail.com>
2017-02-04 14:26:14 -0500

    Modified side bar

--- src/bars/side_bar.jsx
+++ src/bars/side_bar.jsx
@@ -9,12 +9,13 @@ class SideBar extends Component {
     return(
       <aside className="side-bar">
         <h3>About:</h3>
-        <p>JavaScript, HTML, and CSS are three essential tools for web developers.
-        jsTinker allows you to experiment with those tools.
+        <p>JavaScript, HTML, and CSS are three of the most essential tools for web developers.
+        jsTinker allows you to experiment with those tools and hone youre development skills.
         Click the run button at the top to see what's possible.</p>
       <h3>Links:</h3>
         <ul>
-          <li><a href="http://www.github.com/eakman">GitHub</a></li>
+          <li><a href="http://www.github.com/eakman">GitHub Profile</a></li>
+          <li><a href="http://www.github.com/eakman/jstinker">Project Repo</a></li>
           <li><a href="http://www.eitanakman.com">Portfolio</a></li>
         </ul>
         <h3>Contact:</h3>
